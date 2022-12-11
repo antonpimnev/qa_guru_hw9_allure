c3239f91539bf7537735a9ad6615fd02e3f0ac57
Anton Pimnev <anton.pimnev@gmail.com>
2022-12-11 16:20:05 +0500

    Oh no

---
+++ src/test/java/qa/guru/allure/StepsTest.java
@@ -0,0 +1,41 @@
+package qa.guru.allure;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static io.qameta.allure.Allure.step;
+import static org.openqa.selenium.By.linkText;
+
+public class StepsTest {
+
+    private static final String repository = "antonpimnev/qa_guru_hw9_allure";
+    private static final int issue = 1;
+
+    @Test
+    public void testLambdaStep() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+
+        step("Открываем главную страницу", () -> {
+            open("https://github.com");
+        });
+        step("Ищем репозиторий " + repository, () -> {
+            $(".header-search-input").click();
+            $(".header-search-input").sendKeys(repository);
+            $(".header-search-input").submit();
+        });
+        step("Кликаем по ссылке репозитория " + repository, () -> {
+            $(linkText(repository)).click();
+        });
+        step("Открываем таб Issues", () -> {
+            $("#issues-tab").click();
+        });
+        step("Проверяем наличие Issue с номером " + issue, () -> {
+            $(withText("#" + issue)).should(Condition.visible);
+        });
+    }
+}
---
+++ src/test/java/qa/guru/allure/WebSteps.java
@@ -0,0 +1,56 @@
+package qa.guru.allure;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.Step;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static org.openqa.selenium.By.linkText;
+
+public class WebSteps {
+
+    private static final String repo = "antonpimnev/qa_guru_hw9_allure";
+    private static final int issue = 1;
+
+    @Step("Открываем главную страницу")
+    public void openMainPage() {
+        open("https://github.com");
+    }
+
+    @Step("Ищем репозиторий {repo}")
+    public void searchForRepository(String repo) {
+        $(".header-search-input").click();
+        $(".header-search-input").sendKeys(repo);
+        $(".header-search-input").submit();
+    }
+
+    @Step("Кликаем по ссылке репозитория {repo}")
+    public void clickOnRepositoryLink(String repo) {
+        $(linkText(repo)).click();
+    }
+
+    @Step("Открываем таб Issues")
+    public void openIssuesTab() {
+        $("#issues-tab").click();
+    }
+
+    @Step("Проверяем наличие Issue с номером {issue}")
+    public void shouldSeeIssueWithNumber(int issue) {
+        $(withText("#" + issue)).should(Condition.visible);
+    }
+
+    @Test
+    public void annotationsStepTest() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+        WebSteps steps = new WebSteps();
+        steps.openMainPage();
+        steps.searchForRepository(repo);
+        steps.clickOnRepositoryLink(repo);
+        steps.openIssuesTab();
+        steps.shouldSeeIssueWithNumber(issue);
+    }
+}
